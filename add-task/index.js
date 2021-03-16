4f52e44bf23bd7c45924cf7368e995be65820861
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-03-16 16:47:14 -0500

    Validate not empty

--- add-task/index.js
+++ add-task/index.js
@@ -4,27 +4,34 @@ module.exports = async function (context, req) {
     context.log('Adding a task to the planner');
 
     const task = req.body;
-    let dueDate = formatDate(new Date(task.dueDate));
-    let responsible = {
-        name : task.responsible,
-        email : task.email
-    }
-    let taskBody = {
-        description: task.description,
-        status: task.status,
-        dueDate: dueDate,
-        responsible : responsible
-    }
-    
-    let message = "Correct Task Body";
-    let responseCode = 201;
+    let message = "Task Body Empty";
+    let responseCode = 400;
+    let responseMessage = { message: message};
 
-    if (task.responsible == null || task.email == null || task.description == null || task.status == null || dueDate == null){
+    if (typeof(task) != "undefined"){
+
         message = "Bad Task Body";
-        responseCode = 400;
+
+        let dueDate = formatDate(new Date(task.dueDate));
+        let responsible = {
+            name : task.responsible,
+            email : task.email
+        }
+        let taskBody = {
+            description: task.description,
+            status: task.status,
+            dueDate: dueDate,
+            responsible : responsible
+        }
+
+        if (task.responsible != null && task.email != null && task.description != null && task.status != null && dueDate != null){
+            message = "Correct Task Body";
+            responseCode = 201;
+        }
+        
+        responseMessage = { taskBody: taskBody, message: message};
     }
 
-    const responseMessage = { taskBody: taskBody, message: message};
 
     context.res = {
         status: responseCode,
